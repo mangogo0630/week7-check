c8ab840b8ed9a95b0e3c0c7068bf5a93917128b8
mango <mangogo0630@gmail.com>
2024-09-06 00:51:08 +0800

    first commit

---
+++ main copy.js
@@ -0,0 +1,5 @@
+AOS.init();
+
+import './assets/scss/all.scss';
+
+console.log("Hello world!");
---
+++ main.js
@@ -0,0 +1,9 @@
+AOS.init({
+    useClassNames: true,
+    initClassName: false,
+    animatedClassName: 'animate__animated',
+});
+
+import './assets/scss/all.scss';
+
+console.log("Hello world!");
